67a2dc67af7e36bbf8b584231b78c13c937915d6
khushi230 <khushboosirsa99@gmail.com>
2022-12-25 00:07:10 +0530

    article list component moved to common

--- src/pages/articlesList/index.js
+++ src/pages/articlesList/index.js
@@ -1,22 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 import articles from "../../utils/articleContent";
+import ArticlesList from "../../common/ArticlesList";
 
-const ArticlesList = () => {
+const Articles = () => {
   return (
     <div style={{ textAlign: "left" }}>
       <h1>Articles</h1>
-      {articles.map((article) => (
-        <div key={article.name} style={{ marginTop: "50px" }}>
-          <Link to={`${article.name}`}>{article.title}</Link>
-          <div>
-            <p>{article.content[0].substring(0, 150)}</p>
-          </div>
-        </div>
-      ))}
+      <ArticlesList articles={articles} />
     </div>
   );
 };
 
-export default ArticlesList;
+export default Articles;
